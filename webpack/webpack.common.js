212b1310cfe9ef16c5950ea003d5c6cd2f46305f
Matt Raible <matt.raible@okta.com>
2017-10-03 17:51:46 -0700

    Add PWA Support

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -94,7 +94,7 @@ module.exports = (options) => {
                 { from: './src/main/webapp/swagger-ui/', to: 'swagger-ui' },
                 { from: './src/main/webapp/favicon.ico', to: 'favicon.ico' },
                 { from: './src/main/webapp/manifest.webapp', to: 'manifest.webapp' },
-                // { from: './src/main/webapp/sw.js', to: 'sw.js' },
+                { from: './src/main/webapp/sw.js', to: 'sw.js' },
                 { from: './src/main/webapp/robots.txt', to: 'robots.txt' }
             ]),
             new webpack.ProvidePlugin({
